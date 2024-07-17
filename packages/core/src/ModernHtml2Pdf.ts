0abb106072e3dbc41242997848d2f39139eab0f9
Vincent-the-gamer <erxianqiaottjt@163.com>
2024-07-17 17:05:28 +0800

    feat: add playground.

--- packages/core/src/ModernHtml2Pdf.ts
+++ packages/core/src/ModernHtml2Pdf.ts
@@ -8,6 +8,18 @@ export class ModernHtml2Pdf {
         this.el = el
     }
 
-    toCanvas = async (domToCanvasOptions: Record<string, any>) => await domToCanvas(this.el, domToCanvasOptions)
+    toCanvas = async (domToCanvasOptions?: Record<string, any>) => {
+        if (!domToCanvasOptions) {
+            // default options
+            return await domToCanvas(this.el, {
+                backgroundColor: "#ffffff",
+                style: {
+                    color: "black"
+                }
+            })
+        } else {
+            return await domToCanvas(this.el, domToCanvasOptions)
+        }
+    }
     toJSPdf = (jsPDFOptions: jsPDFOptions) => new jsPDF({ ...jsPDFOptions })
 }
